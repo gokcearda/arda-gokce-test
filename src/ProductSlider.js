969a38066873939fba75b39875183e0e95b012e5
Arda <111998578+gokcearda@users.noreply.github.com>
2023-12-11 06:03:31 +0300

    .
    .

--- src/ProductSlider.js
+++ src/ProductSlider.js
@@ -1,11 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import SwiperCore, { Navigation, Pagination } from 'swiper/core';
+import { Navigation, Pagination, Mousewheel, Keyboard } from 'swiper/modules';
 
-// Tüm Swiper modüllerini etkinleştirin
 import 'swiper/swiper-bundle.css';
 
 const ProductSlider = () => {
@@ -18,24 +17,63 @@ const ProductSlider = () => {
     { id: 6, name: 'EG075', price: 19.99 },
     { id: 7, name: 'EG094', price: 19.99 },
     { id: 8, name: 'EG115', price: 19.99 },
-
   ];
 
   const colors = ['W', 'R', 'Y']; // Renk seçenekleri
 
   const [activeProductIndex, setActiveProductIndex] = useState(0);
+  const [slidesPerView, setSlidesPerView] = useState(calculateSlidesPerView());
 
-  const handleProductButtonClick = (index) => {
-    setActiveProductIndex(index);
+  // slidesPerView değerini belirlemek için bir fonksiyon
+  function calculateSlidesPerView() {
+    const screenWidth = window.innerWidth;
+
+    if (screenWidth >= 1200) {
+      return 4;
+    } else if (screenWidth >= 992) {
+      return 3;
+    } else if (screenWidth >= 768) {
+      return 2;
+    } else {
+      return 1;
+    }
+  }
+
+  // calculateSlidesPerView fonksiyonunu useEffect içinde çağırın
+  useEffect(() => {
+    setSlidesPerView(calculateSlidesPerView());
+  }, []);
+
+  const handleProductButtonClick = (productIndex) => {
+    setActiveProductIndex(productIndex);
   };
+
+  useEffect(() => {
+    // pencere boyutu değiştikçe slidesPerView değerini güncelle
+    const handleResize = () => {
+      setSlidesPerView(calculateSlidesPerView());
+    };
+
+    // pencere boyutu değiştiğinde event listener'ı ekle
+    window.addEventListener('resize', handleResize);
+
+    // component unmount olduğunda event listener'ı kaldır
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
     <Swiper
       spaceBetween={20}
       navigation
       pagination={{ clickable: true }}
-      slidesPerView={4}
+      slidesPerView={slidesPerView} // slidesPerView değerini state'ten alın
       loop={true}
+      mousewheel={true}
+      keyboard={true}
+      modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+      className="mySwiper"
     >
       {products.map((product) => (
         <SwiperSlide key={product.id}>
@@ -47,13 +85,13 @@ const ProductSlider = () => {
             <h3>{product.name}</h3>
             <p>Fiyat: ${product.price}</p>
             <div className="product-buttons">
-              {colors.map((color, index) => (
+              {colors.map((colorProduct, index) => (
                 <button
-                  key={color}
-                  className={`product-button ${index === activeProductIndex ? 'active' : ''}`}
+                  key={colorProduct}
+                  className={`product-button ${colorProduct === colors[activeProductIndex] ? 'active' : ''}`}
                   onClick={() => handleProductButtonClick(index)}
                 >
-                  {color}
+                  {colorProduct}
                 </button>
               ))}
             </div>
